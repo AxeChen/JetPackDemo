ef03b7ff7ed9c642dad0b38ce7480315c724556d
axeChen <1137366723@qq.com>
2021-03-31 08:53:39 +0800

    Signed-off-by: axeChen <1137366723@qq.com>

---
+++ room/src/main/java/com/axe/room/bean/Article.kt
@@ -0,0 +1,61 @@
+package com.axe.room.bean
+
+import androidx.room.Entity
+
+
+data class ArticleListBean(
+    var curPage: Int,
+    var datas: MutableList<ArticleBean>,
+    var offset: Int,
+    var over: Boolean,
+    var pageCount: Int,
+    var size: Int,
+    var total: Int
+)
+
+@Entity
+data class ArticleBean(
+    val id: Int,
+    val apkLink: String,
+    val audit: Int,
+    val author: String,
+    val canEdit: Boolean,
+    val chapterId: Int,
+    val chapterName: String,
+    val collect: Boolean,
+    val courseId: Int,
+    val desc: String,
+    val descMd: String,
+    val envelopePic: String,
+    val fresh: Boolean,
+    val link: String,
+    val niceDate: String,
+    val niceShareDate: String,
+    val origin: String,
+    val prefix: String,
+    val projectLink: String,
+    val publishTime: Long,
+    val realSuperChapterId: Int,
+    val selfVisible: Int,
+    val shareDate: Long,
+    val shareUser: String,
+    val superChapterId: Int,
+    val superChapterName: String,
+    val tags: List<Any>,
+    val title: String,
+    val type: Int,
+    val userId: Int,
+    val visible: Int,
+    val zan: Int
+)
+
+
+data class HotWord(
+    var id: Int,
+    var link: String,
+    var name: String,
+    var order: Int,
+    var visible: Int
+)
+
+
---
+++ room/src/main/java/com/axe/room/db/AppDataBase.kt
@@ -0,0 +1,11 @@
+package com.axe.room.db
+
+import androidx.room.RoomDatabase
+
+/**
+ * 数据库类
+ */
+abstract class AppDataBase : RoomDatabase() {
+
+
+}
